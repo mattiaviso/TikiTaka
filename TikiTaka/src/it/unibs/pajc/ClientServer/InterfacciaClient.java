3d86096d6e3930888d24a84c497310b968be773d
AndreaZambetti <80603335+AndreaZambetti@users.noreply.github.com>
2022-03-15 21:18:58 +0100

    Finito Grafica iniziale

--- TikiTaka/src/it/unibs/pajc/ClientServer/InterfacciaClient.java
+++ TikiTaka/src/it/unibs/pajc/ClientServer/InterfacciaClient.java
@@ -1,83 +1,109 @@
 package it.unibs.pajc.ClientServer;
 
 
+import it.unibs.pajc.Client;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-
-import it.unibs.pajc.Client;
-import it.unibs.pajc.Partita.FieldObject;
-
-import java.awt.event.ActionListener;
-import java.net.URL;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.Canvas;
-import java.awt.Panel;
-import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class InterfacciaClient extends JFrame {
 
+	public static final String MSG_NOME = "Inserisci qui il tuo username";
+	public static final String ERRORE_NOME = "INSERIRE IL NOME";
+	public static final String ERRORE_IP = "IP SERVER NON CORRETTO";
 	private JPanel contentPane;
 	private JTextField txtIp;
 	private JTextField txtUser;
 	private JButton btnEsterno;
-	private JPanel panel;
-	
+
 
 	public InterfacciaClient() {
+		ImageIcon img = new ImageIcon("Ball.png");
+		this.setIconImage(img.getImage());
+		this.setTitle("TikiTaka");
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 287);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.WHITE);
+		contentPane.setBackground(Color.GREEN);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		txtIp = new JTextField();
+		txtIp.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				txtIp.setBackground(Color.WHITE);
+				txtIp.setText("");
+			}
+		});
 		txtIp.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		txtIp.setText(" ip server (no localhost)");
-		txtIp.setBounds(10, 76, 342, 36);
+		txtIp.setBounds(10, 76, 386, 36);
 		contentPane.add(txtIp);
 		txtIp.setColumns(10);
-		
+
 		JButton btnLocal = new JButton("LocalHost");
 		btnLocal.setFont(new Font("Arial", Font.BOLD, 14));
 		btnLocal.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(!txtUser.getText().equals("")) {
+				if (!(txtUser.getText().equals("") || txtUser.getText().equals(MSG_NOME) || txtUser.getText().equals(ERRORE_NOME))) {
 					Client.avvioClient(1500, "localhost", txtUser.getText());
 					setVisible(false);
+				} else {
+					txtUser.setText(ERRORE_NOME);
+					txtUser.setBackground(Color.RED);
 				}
 			}
 		});
-		btnLocal.setBounds(10, 142, 125, 47);
+		btnLocal.setBounds(10, 142, 174, 48);
 		contentPane.add(btnLocal);
-		
+
 		txtUser = new JTextField();
+		txtUser.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				txtUser.setText("");
+				txtUser.setBackground(Color.WHITE);
+			}
+		});
 		txtUser.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtUser.setText("Inserisci qui il tuo username");
+		txtUser.setText(MSG_NOME);
 		txtUser.setHorizontalAlignment(SwingConstants.LEFT);
 		txtUser.setBounds(10, 30, 386, 36);
 		contentPane.add(txtUser);
 		txtUser.setColumns(10);
-		
+
 		btnEsterno = new JButton("Server Esterno");
 		btnEsterno.setFont(new Font("Arial", Font.BOLD, 14));
 
 		btnEsterno.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(isValidIp(txtIp.getText()) && !txtUser.getText().equals("") ) {
+				if (isValidIp(txtIp.getText()) && !txtUser.getText().equals("") && !txtUser.getText().equals(MSG_NOME) && !txtUser.getText().equals(ERRORE_NOME)) {
 					Client.avvioClient(1500, txtIp.getText(), txtUser.getText());
 					setVisible(false);
+				} else {
+					if(!isValidIp(txtIp.getText())){
+						txtIp.setText(ERRORE_IP);
+						txtIp.setBackground(Color.RED);
+					}
+					else{
+						txtUser.setText(ERRORE_NOME);
+						txtUser.setBackground(Color.RED);
+					}
+
 				}
 			}
 		});
-		btnEsterno.setBounds(213, 143, 139, 44);
+		btnEsterno.setBounds(235, 143, 180, 47);
 		contentPane.add(btnEsterno);
 	
 	}
